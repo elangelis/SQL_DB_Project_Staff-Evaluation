e97bde6560ffc88ae9f952188a96487b8028bad9
elangelis <73367290+elangelis@users.noreply.github.com>
2021-10-21 17:15:49 +0300

    Add files via upload

--- create.sql
+++ create.sql
@@ -15,10 +15,12 @@ CREATE TABLE user
 DROP TABLE IF EXISTS degree;
 CREATE TABLE degree
 (
-    titlos varchar(50),
-    idryma varchar(40),
+
+    titlos varchar(50) NOT NULL,
+    idryma varchar(40) NOT NULL,
     bathmida ENUM("LYKEIO","UNIV","MASTER","PHD"),
-    PRIMARY KEY (titlos,idryma)
+    PRIMARY KEY (titlos),
+    UNIQUE(idryma)
 );
 
 DROP TABLE IF EXISTS job;
@@ -32,7 +34,10 @@ CREATE TABLE job
     evaluator varchar(12),
     announce_date datetime,
     submission_date date,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+
+    CONSTRAINT WHICHEVL 
+    FOREIGN KEY (evaluator) REFERENCES evaluator(username)   
 );
 
 DROP TABLE IF EXISTS company;
@@ -52,10 +57,10 @@ CREATE TABLE company
 DROP TABLE IF EXISTS antikeim;
 CREATE TABLE antikeim
 (
-    title varchar(36),
+    title varchar(36) NOT NULL,
     descr tinytext,
-    belongs_to varchar(36),
-    PRIMARY KEY (title,belongs_to)
+    belongs_to varchar(36) NOT NULL,
+    PRIMARY KEY (title)
 );
 
 /*_____________________________________CHILD TABLES___________________________________*/
@@ -66,15 +71,13 @@ CREATE TABLE manager
     managerUsername varchar(12) NOT NULL,
     exp_years tinyint(4) DEFAULT "0" NOT NULL,
     firm char(9) DEFAULT "0" NOT NULL,
-    PRIMARY KEY (managerUsername),
 
     CONSTRAINT MNGRUSR                      
-    FOREIGN KEY (managerUsername) REFERENCES user(username)
-    ,
+    FOREIGN KEY (managerUsername) REFERENCES user(username),
 
     CONSTRAINT FIRMAFM                      
     FOREIGN KEY (firm) REFERENCES company(AFM)
-    
+
 );
 
 DROP TABLE IF EXISTS evaluator;
@@ -82,27 +85,24 @@ CREATE TABLE evaluator
 (
     username varchar(12) NOT NULL,
     exp_years tinyint(4) DEFAULT "0" NOT NULL,
-    firm char(9),
-    PRIMARY KEY (username),
+    firm char(9) NOT NULL,
 
     CONSTRAINT EVLTRUSR                      
-    FOREIGN KEY (username) REFERENCES user(username)
-    ,
+    FOREIGN KEY (username) REFERENCES user(username),
 
     CONSTRAINT FIRMAFM2                     
     FOREIGN KEY (firm) REFERENCES company(AFM) 
-    
+
 );
 
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee
 (
-    username varchar(12),
+    username varchar(12) NOT NULL,
     bio text,
     sistatikes varchar(35),
     certificates varchar(35),
     awards varchar(35),
-    PRIMARY KEY (username),
 
     CONSTRAINT EMPLUSR                      
     FOREIGN KEY (username) REFERENCES user(username)
@@ -112,9 +112,8 @@ CREATE TABLE employee
 DROP TABLE IF EXISTS needs;
 CREATE TABLE needs
 (
-     job_id int(4),
-    antikeim_title varchar(36),
-    PRIMARY KEY(job_id,antikeim_title),
+     job_id int(4) NOT NULL,
+    antikeim_title varchar(36) NOT NULL,
 
     CONSTRAINT NDSANTIK
     FOREIGN KEY(antikeim_title) REFERENCES antikeim(title),
@@ -127,11 +126,11 @@ CREATE TABLE needs
 DROP TABLE IF EXISTS project;
 CREATE TABLE project
 (
-    empl varchar(12),
-    num tinyint(4),
+    empl varchar(12)NOT NULL,
+    num tinyint(4)NOT NULL,
     descr text,
     url varchar(60),
-    PRIMARY KEY(empl,num),
+    PRIMARY KEY(num),
 
     CONSTRAINT PROJEMPL
     FOREIGN KEY(empl) REFERENCES employee(username)
@@ -141,9 +140,9 @@ CREATE TABLE project
 DROP TABLE IF EXISTS languages;
 CREATE TABLE languages
 (
-    employee varchar(12),
+    employee varchar(12)NOT NULL,
     lang set("EN","FR","SP","GR"),
-    PRIMARY KEY(employee,lang),
+    PRIMARY KEY(lang),
 
     CONSTRAINT LANGEMPL
     FOREIGN KEY(employee) REFERENCES employee(username)
@@ -153,10 +152,9 @@ CREATE TABLE languages
 DROP TABLE IF EXISTS requestevaluation;
 CREATE TABLE requestevaluation
 (
-    empl_username varchar(12),
-    job_id int(4),
-    PRIMARY KEY(empl_username,job_id),
-    
+    empl_username varchar(12)NOT NULL,
+    job_id int(4)NOT NULL,
+
     CONSTRAINT EMPLRQSTSEVAL
     FOREIGN KEY(empl_username) REFERENCES employee(username),
 
@@ -164,6 +162,23 @@ CREATE TABLE requestevaluation
     FOREIGN KEY (job_id) REFERENCES job(id)
     
 );
+
+DROP TABLE IF EXISTS evaluationresult;
+CREATE TABLE evaluationresult
+(
+    Evlid int(4) NOT NULL,
+    empl_username varchar(12)NOT NULL,
+    job_id int(4)NOT NULL,
+    grade int(4),
+    comments varchar(255),
+
+    CONSTRAINT WHCHJOB
+    FOREIGN KEY (job_id) REFERENCES job(id),
+    
+    CONSTRAINT WHCHEMPL
+    FOREIGN KEY (empl_username) REFERENCES employee(username)
+);
+
 
 DROP TABLE IF EXISTS has_degree;
 CREATE TABLE has_degree 
@@ -173,7 +188,6 @@ CREATE TABLE has_degree
     empl_usrname varchar(12),
     etos year,
     grade float (3,1),
-    PRIMARY KEY(degr_title,degr_idryma,empl_usrname),
 
     CONSTRAINT WHCHDEGREE
     FOREIGN KEY (degr_title) REFERENCES degree(titlos),
@@ -181,26 +195,17 @@ CREATE TABLE has_degree
     CONSTRAINT WHRDEGREE
     FOREIGN KEY (degr_idryma) REFERENCES degree(idryma),
 
-    CONSTRAINT WHCHEMPL
+    CONSTRAINT WHCHEMPL2
     FOREIGN KEY (empl_usrname) REFERENCES employee(username)
 );
-   
-   
-DROP TABLE IF EXISTS evaluationresult;
-CREATE TABLE evaluationresult
+
+DROP TABLE IF EXISTS administrator;
+CREATE TABLE admininstrator
 (
-    Evld int(4),
-    empl_username varchar(12)NOT NULL,
-    job_id int(4),
-    grade int(4),
-    comments varchar(255),
-    PRIMARY KEY(Evld,empl_username),
-
-    CONSTRAINT WHCHJOB
-    FOREIGN KEY (job_id) REFERENCES job(id),
+    administrator_username varchar(12),
     
-    CONSTRAINT WHCHEMPL
-    FOREIGN KEY (Evld) REFERENCES employee(username)
-);
 
-    
+
+
+
+)
