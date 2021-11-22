e27fc027fc0156df673cca265912d723d6297f03
elangelis <73367290+elangelis@users.noreply.github.com>
2021-11-22 20:49:31 +0200

    Add files via upload

--- create.sql
+++ create.sql
@@ -6,8 +6,8 @@ CREATE TABLE user
     username varchar(12) DEFAULT "unknown" NOT NULL,
     pass varchar(10) NOT NULL,
     name varchar(25) DEFAULT "unknown" NOT NULL,
-    reg_date datetime,
-    email varchar (30)DEFAULT "unknown" NOT NULL,
+    reg_date datetime DEFAULT NULL,
+    email varchar (30) DEFAULT "unknown" NOT NULL,
     PRIMARY KEY (username),
     UNIQUE(email)
 );
@@ -18,39 +18,24 @@ CREATE TABLE degree
 
     titlos varchar(50) NOT NULL,
     idryma varchar(40) NOT NULL,
-    bathmida ENUM("LYKEIO","UNIV","MASTER","PHD"),
+    bathmida ENUM("LYKEIO","UNIV","MASTER","PHD") DEFAULT NULL,
     PRIMARY KEY (titlos),
     UNIQUE(idryma)
 );
 
-DROP TABLE IF EXISTS job;
-CREATE TABLE job
-(
-    id int(4) NOT NULL,
-    start_date date,
-    salary float(6,1),
-    position varchar(40),
-    edra varchar(45),
-    evaluator varchar(12),
-    announce_date datetime,
-    submission_date date,
-    PRIMARY KEY(id),
 
-    CONSTRAINT WHICHEVL 
-    FOREIGN KEY (evaluator) REFERENCES evaluator(username)   
-);
 
 DROP TABLE IF EXISTS company;
 CREATE TABLE company
 (
     AFM char(9) NOT NULL,
-    DOY varchar(15),
-    name varchar(35),
-    phone bigint(16),
-    street varchar(15),
-    num tinyint(4),
-    city varchar(15),
-    country varchar(15),
+    DOY varchar(15) NOT NULL,
+    name varchar(35) DEFAULT NULL,
+    phone bigint(16) DEFAULT NULL,
+    street varchar(15) DEFAULT NULL,
+    num tinyint(4) DEFAULT NULL,
+    city varchar(15) DEFAULT NULL,
+    country varchar(15) DEFAULT NULL,
     PRIMARY KEY (AFM)
 );
 
@@ -58,8 +43,8 @@ DROP TABLE IF EXISTS antikeim;
 CREATE TABLE antikeim
 (
     title varchar(36) NOT NULL,
-    descr tinytext,
-    belongs_to varchar(36) NOT NULL,
+    descr tinytext DEFAULT NULL,
+    belongs_to varchar(36) DEFAULT "no upper category",
     PRIMARY KEY (title)
 );
 
@@ -73,10 +58,12 @@ CREATE TABLE manager
     firm char(9) DEFAULT "0" NOT NULL,
 
     CONSTRAINT MNGRUSR                      
-    FOREIGN KEY (managerUsername) REFERENCES user(username),
+    FOREIGN KEY (managerUsername) REFERENCES user(username)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
     CONSTRAINT FIRMAFM                      
     FOREIGN KEY (firm) REFERENCES company(AFM)
+    ON UPDATE CASCADE ON DELETE CASCADE
 
 );
 
@@ -88,10 +75,12 @@ CREATE TABLE evaluator
     firm char(9) NOT NULL,
 
     CONSTRAINT EVLTRUSR                      
-    FOREIGN KEY (username) REFERENCES user(username),
+    FOREIGN KEY (username) REFERENCES user(username)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
     CONSTRAINT FIRMAFM2                     
-    FOREIGN KEY (firm) REFERENCES company(AFM) 
+    FOREIGN KEY (firm) REFERENCES company(AFM)
+    ON UPDATE CASCADE ON DELETE CASCADE
 
 );
 
@@ -99,14 +88,32 @@ DROP TABLE IF EXISTS employee;
 CREATE TABLE employee
 (
     username varchar(12) NOT NULL,
-    bio text,
-    sistatikes varchar(35),
-    certificates varchar(35),
-    awards varchar(35),
+    bio text DEFAULT NULL,
+    sistatikes varchar(35) DEFAULT NULL,
+    certificates varchar(35) DEFAULT NULL,
+    awards varchar(35) DEFAULT NULL,
 
     CONSTRAINT EMPLUSR                      
     FOREIGN KEY (username) REFERENCES user(username)
-    
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS job;
+CREATE TABLE job
+(
+    id int(4) NOT NULL,
+    start_date date DEFAULT NULL,
+    salary float(6,1) DEFAULT NULL,
+    position varchar(40) DEFAULT NULL,
+    edra varchar(45) DEFAULT NULL,
+    evaluator varchar(12) DEFAULT NULL,
+    announce_date datetime DEFAULT NULL,
+    submission_date date DEFAULT NULL,
+    PRIMARY KEY(id),
+
+    CONSTRAINT WHICHEVL 
+    FOREIGN KEY (evaluator) REFERENCES evaluator(username)
+    ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 DROP TABLE IF EXISTS needs;
@@ -116,10 +123,12 @@ CREATE TABLE needs
     antikeim_title varchar(36) NOT NULL,
 
     CONSTRAINT NDSANTIK
-    FOREIGN KEY(antikeim_title) REFERENCES antikeim(title),
+    FOREIGN KEY(antikeim_title) REFERENCES antikeim(title)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
     CONSTRAINT JOBNEEDS
     FOREIGN KEY(job_id) REFERENCES job(id)
+    ON UPDATE CASCADE ON DELETE CASCADE
 
 );
 
@@ -127,13 +136,15 @@ DROP TABLE IF EXISTS project;
 CREATE TABLE project
 (
     empl varchar(12)NOT NULL,
-    num tinyint(4)NOT NULL,
-    descr text,
-    url varchar(60),
+    num tinyint(4) AUTO_INCREMENT,
+    descr text DEFAULT "no entry yet",
+    url varchar(60) NOT NULL,
     PRIMARY KEY(num),
+    UNIQUE (url),
 
     CONSTRAINT PROJEMPL
     FOREIGN KEY(empl) REFERENCES employee(username)
+    ON UPDATE CASCADE ON DELETE CASCADE
 
 );
 
@@ -141,11 +152,12 @@ DROP TABLE IF EXISTS languages;
 CREATE TABLE languages
 (
     employee varchar(12)NOT NULL,
-    lang set("EN","FR","SP","GR"),
-    PRIMARY KEY(lang),
+    lang set("EN","FR","SP","GR") NOT NULL,
+    PRIMARY KEY(employee),
 
     CONSTRAINT LANGEMPL
     FOREIGN KEY(employee) REFERENCES employee(username)
+    ON UPDATE CASCADE ON DELETE CASCADE
 
 );
 
@@ -154,58 +166,93 @@ CREATE TABLE requestevaluation
 (
     empl_username varchar(12)NOT NULL,
     job_id int(4)NOT NULL,
+    EvID int NOT NULL,
+    PRIMARY KEY (EvID),
 
     CONSTRAINT EMPLRQSTSEVAL
-    FOREIGN KEY(empl_username) REFERENCES employee(username),
+    FOREIGN KEY(empl_username) REFERENCES employee(username)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
     CONSTRAINT FORJOB
     FOREIGN KEY (job_id) REFERENCES job(id)
+    ON UPDATE CASCADE ON DELETE CASCADE
     
+);
+
+DROP TABLE IF EXISTS evaluationphases;
+CREATE TABLE evaluationphases
+(
+    EvID int (4),
+    phase1 int (4),
+    phase2 int (4),
+    phase3 int (4),
+    
+    FOREIGN KEY (EvID) REFERENCES requestevaluation(EvID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+
+    CHECK ((phase1>=0) AND (phase1<=4) AND (phase2>=0) AND (phase2<=4) AND (phase3>=0) AND (phase3<=2))
+
 );
 
 DROP TABLE IF EXISTS evaluationresult;
 CREATE TABLE evaluationresult
 (
-    Evlid int(4) NOT NULL,
-    empl_username varchar(12)NOT NULL,
-    job_id int(4)NOT NULL,
-    grade int(4),
-    comments varchar(255),
+    EvId int(4) NOT NULL,
+    empl_username varchar(12) NOT NULL,
+    job_id int(4) NOT NULL,
+    grade int(4) DEFAULT NULL,
+    comments varchar(255) DEFAULT "no entry yet",
+    evaluator varchar(15) DEFAULT NULL,
+
+    CONSTRAINT EVALUATOR 
+    FOREIGN KEY (evaluator) REFERENCES evaluator(username)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+
+    CONSTRAINT WHICHEVID 
+    FOREIGN KEY (EvId) REFERENCES requestevaluation (EvID)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
     CONSTRAINT WHCHJOB
-    FOREIGN KEY (job_id) REFERENCES job(id),
+    FOREIGN KEY (job_id) REFERENCES job(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
     
     CONSTRAINT WHCHEMPL
     FOREIGN KEY (empl_username) REFERENCES employee(username)
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 DROP TABLE IF EXISTS has_degree;
 CREATE TABLE has_degree 
 (
-    degr_title varchar(50),
-    degr_idryma varchar(40),
-    empl_usrname varchar(12),
-    etos year,
-    grade float (3,1),
+    degr_title varchar(50) DEFAULT NULL,
+    degr_idryma varchar(40) DEFAULT NULL,
+    empl_usrname varchar(12) DEFAULT NULL,
+    etos year DEFAULT NULL,
+    grade float (3,1) DEFAULT NULL,
 
     CONSTRAINT WHCHDEGREE
-    FOREIGN KEY (degr_title) REFERENCES degree(titlos),
+    FOREIGN KEY (degr_title) REFERENCES degree(titlos)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
     CONSTRAINT WHRDEGREE
-    FOREIGN KEY (degr_idryma) REFERENCES degree(idryma),
+    FOREIGN KEY (degr_idryma) REFERENCES degree(idryma)
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
-    CONSTRAINT WHCHEMPL2
+    CONSTRAINT WHCHEMPL4
     FOREIGN KEY (empl_usrname) REFERENCES employee(username)
+    ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS administrator;
-CREATE TABLE admininstrator
+CREATE TABLE administrator
 (
-    administrator_username varchar(12),
+    administrator_username varchar(12) NOT NULL,
+    privilegies set("YES","NO"),
+    PRIMARY KEY(administrator_username),
     
+    CONSTRAINT ADMNUSR
+    FOREIGN KEY (administrator_username) REFERENCES user(username)
+    ON UPDATE CASCADE ON DELETE CASCADE
 
-
-
-
-)
+);
