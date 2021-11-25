6ed0ea7110c5ed380d7725cbda7ab7e8a8aea8d1
DamiMikonos <73346345+DamiMikonos@users.noreply.github.com>
2021-11-25 20:42:42 +0200

    Update stored procedures.sql

--- stored procedures.sql
+++ stored procedures.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE PROCEDURE ApplicationsSearch(username varchar(45))
+CREATE PROCEDURE ApplicationsSearch(IN username varchar(45))
 BEGIN
 SELECT EvID FROM requestevaluation WHERE empl_username=username;
 SELECT EvID FROM evaluationresult WHERE empl_username=username AND grade>0;
@@ -9,7 +9,7 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE EvPhases (JobID int, Evaluator varchar(15))
+CREATE PROCEDURE EvPhases (IN JobID int, IN Evaluator varchar(15))
 BEGIN
 IF phase1<>null AND phase2<>null AND phase3<>null THEN
 SET evaluationresult.grade= evaluationphases.phase1 + evaluationphases.phase2 + evaluationphases.phase3; END IF;
@@ -17,7 +17,7 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE PROCEDURE Evaluations (JobID int)
+CREATE PROCEDURE Evaluations (IN JobID int)
 BEGIN
 IF grade<>null THEN
 SELECT 'Final Tables';
