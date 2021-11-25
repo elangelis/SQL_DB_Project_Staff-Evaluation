0e8a4d28325c820976fc914c325df45cb8f2ab23
DamiMikonos <73346345+DamiMikonos@users.noreply.github.com>
2021-11-25 20:40:06 +0200

    Update stored procedures.sql

--- stored procedures.sql
+++ stored procedures.sql
@@ -9,6 +9,14 @@ END$$
 DELIMITER ;
 
 DELIMITER $$
+CREATE PROCEDURE EvPhases (JobID int, Evaluator varchar(15))
+BEGIN
+IF phase1<>null AND phase2<>null AND phase3<>null THEN
+SET evaluationresult.grade= evaluationphases.phase1 + evaluationphases.phase2 + evaluationphases.phase3; END IF;
+END$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE PROCEDURE Evaluations (JobID int)
 BEGIN
 IF grade<>null THEN
