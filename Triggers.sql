efb9731ce49f585acfd973fed71d1c2c219e052e
DamiMikonos <73346345+DamiMikonos@users.noreply.github.com>
2021-11-26 19:26:39 +0200

    Update Triggers.sql

--- Triggers.sql
+++ Triggers.sql
@@ -1,56 +1,73 @@
-CREATE TRIGGER ai_job AFTER INSERT ON job
-FOR EACH ROW
+DELIMITER $
+DROP TRIGGER IF EXISTS ai_job$
+CREATE TRIGGER ai_job AFTER INSERT ON job FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Insert', 'Job', now());
-CREATE TRIGGER au_job AFTER UPDATE ON job
-FOR EACH ROW
+END$
+DROP TRIGGER IF EXISTS au_job$
+CREATE TRIGGER au_job AFTER UPDATE ON job FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Update', 'Job', now());
-CREATE TRIGGER ad_job AFTER DELETE ON job
-FOR EACH ROW
+END$
+DROP TRIGGER IF EXISTS ad_job$
+CREATE TRIGGER ad_job AFTER DELETE ON job FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Delete', 'Job', now());
+END$
 
-CREATE TRIGGER ai_employee AFTER INSERT ON employee
-FOR EACH ROW
+DROP TRIGGER IF EXISTS ai_employee$
+CREATE TRIGGER ai_employee AFTER INSERT ON employee FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Insert', 'Employee', now());
-CREATE TRIGGER au_employee AFTER UPDATE ON employee
-FOR EACH ROW
+END$
+DROP TRIGGER IF EXISTS au_employee$
+CREATE TRIGGER au_employee AFTER UPDATE ON employee FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Update', 'Employee', now());
-CREATE TRIGGER ad_employee AFTER DELETE ON employee
-FOR EACH ROW
+END$
+DROP TRIGGER IF EXISTS ad_employee$
+CREATE TRIGGER ad_employee AFTER DELETE ON employee FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Delete', 'Employee', now());
+END$
 
-CREATE TRIGGER ai_request AFTER INSERT ON requestevaluation
-FOR EACH ROW
+DROP TRIGGER IF EXISTS ai_request$
+CREATE TRIGGER ai_request AFTER INSERT ON requestevaluation FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Insert', 'RequestEvaluation', now());
-CREATE TRIGGER au_request AFTER UPDATE ON requestevaluation
-FOR EACH ROW
+END$
+DROP TRIGGER IF EXISTS au_request$
+CREATE TRIGGER au_request AFTER UPDATE ON requestevaluation FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Update', 'RequestEvaluation', now());
-CREATE TRIGGER ad_request AFTER DELETE ON requestevaluation
-FOR EACH ROW
+END$
+DROP TRIGGER IF EXISTS ad_request$
+CREATE TRIGGER ad_request AFTER DELETE ON requestevaluation FOR EACH ROW
+BEGIN
 INSERT INTO log (`Event`, `Table_Target`, `Date`)
 VALUES ('Delete', 'RequestEvaluation', now());
+END $
 
-DELIMITER $$
+DROP TRIGGER IF EXISTS bu_company$
 CREATE TRIGGER bu_company BEFORE UPDATE ON company FOR EACH ROW 
 BEGIN
 IF NEW.AFM<>OLD.AFM THEN SET NEW.AFM=OLD.AFM; END IF;
 IF NEW.DOY<>OLD.DOY THEN SET NEW.DOY=OLD.DOY; END IF;
 IF NEW.name<>OLD.name THEN SET NEW.name=OLD.name; END IF;
-END $$
-DELIMITER ;
+END $
 
-DELIMITER $$
+DROP TRIGGER IF EXISTS bu_user$
 CREATE TRIGGER bu_user BEFORE UPDATE ON user FOR EACH ROW 
 BEGIN
 IF NEW.username<>OLD.username THEN 
 	SIGNAL SQLSTATE '45000'
     SET MESSAGE_TEXT = 'This update is not permited'; END IF;
-END $$
-DELIMITER ;
+END $
